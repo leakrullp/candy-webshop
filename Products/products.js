d24ade6f83d68ffb77b88448818178fffe7706c7
leakrullp <leakrull96@gmail.com>
2026-02-16 15:36:58 +0100

    Create dummy data

---
+++ Products/products.js
@@ -0,0 +1,65 @@
+const candyProducts = [
+  {
+    price: 25.5,
+    name: "Frugt Vingummibamser",
+    country: "Germany",
+    weight: 250, // Vægt i gram
+    color: ["Red", "Green", "Yellow", "Orange"],
+    discount: 0, // Procent rabat
+    brand: "Haribo",
+    category: "Vingummi",
+    allergies: ["None"],
+    dateAdded: "2023-10-15",
+  },
+  {
+    price: 45.0,
+    name: "Cremet Mælkechokolade",
+    country: "Switzerland",
+    weight: 100,
+    color: ["Brown"],
+    discount: 15, // 15% rabat
+    brand: "Lindt",
+    category: "Chokolade",
+    allergies: ["Mælk", "Nødder", "Soja"],
+    dateAdded: "2023-11-01",
+  },
+  {
+    price: 18.0,
+    name: "Sure Jordbær Snører",
+    country: "Denmark",
+    weight: 120,
+    color: ["Red"],
+    discount: 0,
+    brand: "Malaco",
+    category: "Lakrids/Snører",
+    allergies: ["Gluten"],
+    dateAdded: "2023-11-10",
+  },
+  {
+    price: 32.0,
+    name: "Salte Heksehyl",
+    country: "Netherlands",
+    weight: 200,
+    color: ["Black", "White"],
+    discount: 5,
+    brand: "Toms",
+    category: "Lakrids",
+    allergies: ["Gluten"],
+    dateAdded: "2023-09-20",
+  },
+  {
+    price: 15.0,
+    name: "Klassiske Bolcher",
+    country: "Denmark",
+    weight: 150,
+    color: ["Pink", "White"],
+    discount: 0,
+    brand: "Nørregade",
+    category: "Bolcher",
+    allergies: ["None"],
+    dateAdded: "2023-11-20",
+  },
+];
+
+// Eksempel på at printe data ud i konsollen:
+console.log(candyProducts);
