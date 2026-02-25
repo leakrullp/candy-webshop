5d1e98a309336405c362bb500772b41b7d4dae97
Christina Polyzogopoulou <christinapol09@gmail.com>
2026-02-25 19:33:17 +0100

    products + product detail pages

---
+++ navbar.js
@@ -0,0 +1,3 @@
+var navbar = document.getElementById("navbar");
+
+navbar.innerHTML = '<a href="../index.html">Home</a>' + '<a href="../Products/products.html">Products</a>' + '<a href="#">Login</a>' + '<a href="../Contact/contact.html">Contact</a>';
