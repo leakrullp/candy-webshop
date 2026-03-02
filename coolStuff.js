a050c317c151751747eeb570842b92caecdf6237
kdre <kdre@itu.dk>
2026-03-02 19:16:51 +0100

    cartMSG

--- coolStuff.js
+++ coolStuff.js
@@ -4,3 +4,9 @@ function welcomeMSG() {
     displayElement.innerText = displayName ? `Welcome to Europe's central Candy MEGA MEGA Store, ${displayName}!!` : "Welcome to Europe's central Candy MEGA MEGA Store!!";
 }
 welcomeMSG();
+
+function cartMSG(){
+    const displayName = localStorage.getItem("fname")
+    const displayElement = document.getElementById("cart-header")
+    displayElement.innerText = displayName ? `You have a great taste in candy, ${displayName}` : "You will regret an empty basket on friday!";
+}
