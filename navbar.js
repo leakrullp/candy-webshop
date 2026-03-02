11eb72b6f77aac18ea89d91d5868eef2b32acd47
leakrullp <leakrull96@gmail.com>
2026-03-02 15:22:04 +0100

    Streamline navbars

--- navbar.js
+++ navbar.js
@@ -1,3 +1,7 @@
 var navbar = document.getElementById("navbar");
 
-navbar.innerHTML = '<a href="../index.html">Home</a>' + '<a href="../Products/products.html">Products</a>' + '<a href="#">Login</a>' + '<a href="../Contact/contact.html">Contact</a>';
+navbar.innerHTML =
+  '<a href="../index.html">Home</a>' +
+  '<a href="../Products/products.html">Products</a>' +
+  '<a href="#">Login</a>' +
+  '<a href="#">Cart</a>';
