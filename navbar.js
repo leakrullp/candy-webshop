3bc6567b947f7db2f86a0babb0814dfbd61c2a21
Christina Polyzogopoulou <christinapol09@gmail.com>
2026-03-03 01:06:11 +0100

    added basket functionality

--- navbar.js
+++ navbar.js
@@ -9,5 +9,5 @@ navbar.innerHTML = `
   <a href="../index.html">Home</a>
   <a href="../Products/products.html">Products</a>
   <a class="${logClass}" href="../login.html">${userId}</a>
-  <a href="#">Cart</a>
+  <a href="../basket.html">Cart</a>
 `;
