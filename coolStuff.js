42ad62c210430bb4c802420917bf03a3055884c3
kdre <kdre@itu.dk>
2026-03-12 08:52:47 +0100

    Adding personal text on basket page

--- coolStuff.js
+++ coolStuff.js
@@ -1,12 +1,17 @@
 function welcomeMSG() {
-    const displayName = localStorage.getItem("fname")
-    const displayElement = document.getElementById("welcome-header")
-    displayElement.innerText = displayName ? `Welcome to a world of candy, ${displayName}!!` : "Welcome to a world of candy";
+    const displayName = localStorage.getItem("fname");
+    const displayElement = document.getElementById("welcome-header");
+    if (displayElement) {
+        displayElement.innerText = displayName ? `Welcome to a world of candy, ${displayName}!!` : "Welcome to a world of candy";
+    }
 }
 welcomeMSG();
 
 function cartMSG(){
-    const displayName = localStorage.getItem("fname")
-    const displayElement = document.getElementById("cart-header")
-    displayElement.innerText = displayName ? `You have a great taste in candy, ${displayName}` : "You will regret an empty basket on friday!";
+    const displayName = localStorage.getItem("fname");
+    const displayElement = document.getElementById("cart-header");
+    if (displayElement) {
+        displayElement.innerText = displayName ? `Your weekend will thank you for your purchase, ${displayName}` : "Your weekend will thank you for your purchase";
+    }
 }
+cartMSG();
