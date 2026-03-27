99b186f472abb51b2151c9cd52f083dd4c25353d
Christina Polyzogopoulou <christinapol09@gmail.com>
2026-03-28 00:37:09 +0100

    Get all details about a specific product

--- server.js
+++ server.js
@@ -2,9 +2,28 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+const fs = require('fs');
+
+//helper function to read data from the JSON file
+const getData = () => {
+    const jsonData = fs.readFileSync('./Products/products.json', 'utf8');
+    return JSON.parse(jsonData);
+};
+
 // Test route
 app.get('/', (req, res) => {
   res.send('Server is running');
+});
+
+// get all details about a specific product
+app.get('/products/:id', (req, res) => {
+  const data = getData();
+  const product = data.products.find(p => p.id === parseInt(req.params.id));
+   if (product) {
+    res.json(product); 
+  } else {
+    res.status(404).json({ message: "Candy not found" });
+  }
 });
 
 app.listen(3000, () => {
