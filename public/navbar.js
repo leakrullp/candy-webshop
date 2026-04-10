a5674f8cdd6a4adff44d21a6746f42079b6ec4d5
leakrullp <leakrull@proton.me>
2026-04-10 13:14:02 +0200

    Refactor API endpoint paths and update script references for consistency

--- public/navbar.js
+++ public/navbar.js
@@ -2,16 +2,15 @@ import { numberOfProductsInCart } from "./Products/cartUtils.js";
 
 var navbar = document.getElementById("navbar");
 
-const user = localStorage.getItem("fname")
+const user = localStorage.getItem("fname");
 let userId = user ? user.slice(0, 2).toUpperCase() : "Login";
 let logClass = user ? "profText" : "";
 
-
 navbar.innerHTML = `
-  <a href="../index.html">Home</a>
-  <a href="../Products/products.html">Products</a>
-  <a class="${logClass}" href="../login.html">${userId}</a>
-  <a href="../basket.html"> Cart <span id="cart-count" class="cart-count">0</span> </a>
+  <a href="./index.html">Home</a>
+  <a href="./Products/products.html">Products</a>
+  <a class="${logClass}" href="./login.html">${userId}</a>
+  <a href="./basket.html"> Cart <span id="cart-count" class="cart-count">0</span> </a>
 `;
 
 document.getElementById("cart-count").textContent = numberOfProductsInCart();
